eb54471538c5621ceebf781bef92351fa9534197
David Eyraud <antigenx@waylon.home.eyraud.net>
2016-05-28 19:27:32 -0400

    website changes required update of plugin. swiched from regex to dom parsing while i was at it.

--- plugin.video.gayboystube/addon.py
+++ plugin.video.gayboystube/addon.py
@@ -10,7 +10,18 @@ import re
 import xbmcplugin
 import xbmcgui
 import xbmcaddon
-from BeautifulSoup import MinimalSoup as BeautifulSoup
+import CommonFunctions
+import cookielib
+
+common = CommonFunctions
+common.plugin = "plugin.video.gayboystube-1.0.1"
+cookiejar = cookielib.LWPCookieJar()
+cookie_handler = urllib2.HTTPCookieProcessor(cookiejar)
+opener = urllib2.build_opener(cookie_handler)
+
+# debugging
+common.dbg = False #Default
+common.dbglevel = 3 # Default = 3
 
 # xbmc hooks
 __settings__ = xbmcaddon.Addon( id="plugin.video.gayboystube" )
@@ -45,48 +56,43 @@ def displayRootMenu():
     # todo: channels; search
 
 
-def addListItem(name,url,mode,iconimage,page="page1.html"):
+def addListItem(name,url,mode,iconimage,page="page1.html",duration=0):
     u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&thumb="+urllib.quote_plus(iconimage)+"&page="+urllib.quote_plus(page)
     ok=True
-    name=BeautifulSoup(name, convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
     if mode=='scrapeVideoList':
         liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-    elif mode=='playVideo':
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-    liz.setInfo( type="Video", infoLabels={ "Title": name } )
-    if mode=='scrapeVideoList':
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
     elif mode=='playVideo':
+        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "duration": duration } )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
     return ok
 
 
 def indexVideos(path,page):
-    req = urllib2.Request(base_url + path + page)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-    response = urllib2.urlopen(req)
-    link=response.read()
-    response.close()
-
-#	match=re.compile('<a href="{url}(videos/.*\.html)"><img class="img" src="(.*\.jpg)" alt="(.*)" id='.format(url=base_url)).findall(link)
-    match=re.compile('this.src="(?P<thumbnail>http://cdn.gayboystube.com/thumbs/[A-Z0-9a-z/. -_]*[wmvaiflp4]{3}-[0-9].jpg)";\' width="[0-9]{3}" height="[0-9]{3}" alt="(?P<name>[A-Za-z0-9!-@#$%^&*(),.;:\' ]*)" />\r\n\r\n\t\t\t</a>\r\n\r\n\t<a href="http://www.gayboystube.com/(?P<url>video/[0-9]{5,7}/[0-9a-z-]*)" class="title"').findall(link)
-
-    for thumbnail,name,url in match:
-        addListItem(name,base_url + url,'playVideo',urllib.quote(thumbnail,':/'),page)
-
-    match2=re.search('<a href=\'(?P<pge>page[0-9]{1,5}\.html)\' class="next">Next</a>', link)
-    if match2 is not None:
-        addListItem('Go to next page (' + ')',path,'scrapeVideoList','DefaultFolder.png',match2.group('pge'))
+     result = common.fetchPage({"link": base_url + path + page})
+     if result["status"] != 200:
+          print "bad url passed to function indexVideos"
+     content = result["content"]
+     items = common.parseDOM(content, "div", attrs = {"class": "item"})
+     link = common.parseDOM(items, "a", attrs = {"class": ""}, ret="href")
+     image = common.parseDOM(items, "img", ret="src")
+     title = common.parseDOM(items, "img", ret="alt")
+     length = common.parseDOM(items, "div", attrs = {"class": "duration"})
+     for i, link2 in enumerate(link):
+          name = common.makeAscii(common.replaceHTMLCodes(title[i]))
+          addListItem(name=name, url=urllib.quote_plus(link2), mode='playVideo', iconimage=image[i], page=page, duration=length[i])
+     nextpage = common.parseDOM(content, "a", attrs={"class": "next"}, ret="href")
+     addListItem(name="Go to next page", url=path, mode='scrapeVideoList', iconimage='DefaultFolder.png', page=nextpage[len(nextpage)-1])
 
 
 def playVideo(url,name,thumb):
-    req = urllib2.Request(url)
+    req = urllib2.Request(urllib.unquote_plus(url))
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
     response = urllib2.urlopen(req)
     link=response.read()
     response.close()
-    match=re.compile('clip: {\r\n\t{7,9}url: \'([A-Za-z0-9_/.:-?&= _]*)\',').findall(link)
-    name=BeautifulSoup(urllib.unquote_plus(name), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
+    match=re.compile('clip: {\r\n\s*url: \'([A-Za-z0-9_/.:-?&= _]*)\',').findall(link)
     for url in match:
         listitem = xbmcgui.ListItem(name)
         listitem.setInfo('video', {'Title': name, 'Genre': 'Porn'})
@@ -125,6 +131,7 @@ try:
     page=urllib.unquote_plus(params["page"])
 except:
     pass
+
 
 # ok - the parameters are initialized where are we scraping?
 base_url='http://www.gayboystube.com/'
